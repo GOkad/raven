ea25777d8b1eea4e99f8bf898f83e7423523c777
gokadarov <george.okadarov@gmail.com>
2023-02-23 07:53:13 +0200

    Fresh start

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <Arduino.h>
 
-#include "config/HeadPinConfig.h"
-#include "AppCore.h"
-
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
-  static AppCore core(HeadPinConfig{});
 }
